08aaad095314fc209d659a850f39357f99271b3f
Anton Pashkovskyi <125904314+antonpashdev@users.noreply.github.com>
2023-05-22 19:12:20 +0300

    Update [id].tsx

--- pages/items/[id].tsx
+++ pages/items/[id].tsx
@@ -25,6 +25,7 @@ const products = [
       src: "https://i.ibb.co/r2gmzxj/image.png",
       alt: "The Book",
     },
+    orgId: "646669d9f6e214cd84c4453e",
     wallet: "37ZnwAwMShAuFJB6Fahu1TmY6fpb9xAZ6bJKiA38Xnx5",
   },
 ];
@@ -40,6 +41,7 @@ interface Product {
   image: string;
   imageAlt: string;
   wallet: string;
+  orgId: string;
 }
 
 const Item = () => {
